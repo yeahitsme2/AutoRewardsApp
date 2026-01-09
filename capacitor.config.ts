55b22d3d65409c94068cb15d62a2381198607f1d
yeahitsme2 <seymour40@ymail.com>
2026-01-09 17:17:18 -0500

    Updated capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,14 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.autoshop.manager',
   appName: 'Auto Shop Manager',
-  webDir: 'dist',
-  server: {
-    androidScheme: 'https',
-    cleartext: true
-  },
-  android: {
-    allowMixedContent: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
